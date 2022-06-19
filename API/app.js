86c90b5d74cbf55f4dff11c4d5f3ccafac54485a
Flavia Bulad <flavia.bulad@gmail.com>
2022-06-19 18:34:01 -0300

    feat: add post /sign-up

--- API/app.js
+++ API/app.js
@@ -3,10 +3,34 @@ import cors from 'cors';
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
-app.get('/hello', (req, res) => {
-    res.send("Hello World!");
-  });
+const users = [];
+const tweets = [];
+
+app.post('/sign-up', (req, res) => {
+ const user = {
+    username: req.body.username,
+    avatar: req.body.avatar
+ }
+ users.push(user);
+ res.send(req.body);
+});
+
+app.post("/tweets", (req, res) => {
+    const tweet = {
+        username: req.body.username,
+        avatar: req.body.avatar,
+        tweet: req.body.tweet
+    }
+    
+    tweets.unshift(tweet);
+    res.send("OK");
+
+});
+
+
+
 
 app.listen(5000, () => {
     console.log("Server is running");
